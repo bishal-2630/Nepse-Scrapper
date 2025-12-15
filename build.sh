b1f7865ac84a9b33bd4c5a40989f8d15bc9c42ab
Bishal <kbishal177@example.com>
2025-12-15 13:40:48 +0545

    Fixed errors

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 set -o errexit
 
 # Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Fix: Reinstall setuptools to ensure pkg_resources is available
+pip install --force-reinstall -U setuptools
 
 # Collect static files
 python manage.py collectstatic --no-input
