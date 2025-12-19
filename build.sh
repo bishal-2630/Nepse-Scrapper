e9247fa76cd50ff6f0ec77ada65df82cfa282038
Bishal <kbishal177@example.com>
2025-12-19 20:22:54 +0545

    Updated versions

--- build.sh
+++ build.sh
@@ -4,15 +4,10 @@ set -o errexit
 
 echo "=== Starting NEPSE Scraper build ==="
 
-# Remove these lines (or comment them out):
-# echo "Installing system dependencies..."
-# apt-get update
-# apt-get install -y libxml2-dev libxslt-dev python3-dev gcc g++
-
 # Install Python packages
 echo "Installing Python packages..."
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
 # Run Django setup
 echo "Running Django setup..."
