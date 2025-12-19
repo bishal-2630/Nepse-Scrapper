feaa749e0b9bb7eb750e5ebd44f98e1cbbf8e46d
Bishal <kbishal177@example.com>
2025-12-19 20:32:40 +0545

    Updated versions

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@ set -o errexit
 
 echo "=== Starting NEPSE Scraper build ==="
 
-# Install Python packages
 echo "Installing Python packages..."
 pip install --upgrade pip
-pip install --no-cache-dir -r requirements.txt
+pip install -r requirements.txt
 
-# Run Django setup
 echo "Running Django setup..."
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
