6fceba4eb4111d6e41a2a34d61137ff980e797fe
Bishal <kbishal177@example.com>
2025-12-20 00:48:43 +0545

    Fixed Errors for Deployment

--- nepse_scraper/settings.py
+++ nepse_scraper/settings.py
@@ -184,9 +184,4 @@ LOGGING = {
     },
 }
 
-# Security settings for production (Render)
-if not DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = False
